e318692d848ec3af43c54c4f1d7f8fe68d706680
OLEG MANDRYCHENKO <mandrik89@gmail.com>
2021-03-05 20:28:10 +0200

    update 1.01

--- python/src/app.py
+++ python/src/app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@application.route("/")
+def root():
+    return render_template("index.html")
 
 @app.route("/hello")
 def hello():
     return "Hello World!"
-
-
-@application.route("/")
-def root():
-    return render_template("index.html")
